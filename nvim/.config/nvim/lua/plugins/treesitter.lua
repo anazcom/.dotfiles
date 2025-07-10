709eda01565037a394b91d46ca72b20345b52f8e
anazcom <tonito.nazco@gmail.com>
2025-07-10 18:42:50 -0400

    feat(treesitter) updated config in tressitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,71 +1,68 @@
----@type PluginSpec
+---@type LazyPluginSpec
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
+	version = false, -- last release is way too old and doesn't work on Windows
 	build = ":TSUpdate",
-	opts = function()
+	event = { "VeryLazy" },
+	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	opts_extend = { "ensure_installed" },
+	opts = {
+		ensure_installed = {
+			"angular",
+			"bash",
+			"diff",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"printf",
+			"python",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"xml",
+			"yaml",
+			"java",
+			"bash",
+			"graphql",
+		},
 
-        vim.api.nvim_create_autocmd("FileType", {
-			pattern = "htmlangular",
-			callback = function()
-				vim.treesitter.language.register("angular", "htmlangular") -- Register the filetype with treesitter for the `angular` language/parser
-			end,
-		})
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = true,
 
-		return {
-			ensure_installed = {
-				"angular",
-				"bash",
-				"diff",
-				"html",
-				"javascript",
-				"jsdoc",
-				"json",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"printf",
-				"python",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-				"java",
-				"bash",
-				"graphql",
-			},
+		-- List of parsers to ignore installing (or "all")
+		ignore_install = {},
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = true,
+		---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+		-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = false,
+		highlight = {
+			enable = true,
 
-			-- List of parsers to ignore installing (or "all")
-			ignore_install = {},
+			-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+			-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+			-- the name of the parser)
+			-- list of language that will be disabled
+			disable = {},
 
-			---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+			-- Using this option may slow down your editor, and you may see some duplicate highlights.
+			-- Instead of true it can also be a list of languages
+			additional_vim_regex_highlighting = false,
+		},
 
-			highlight = {
-				enable = true,
-
-				-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-				-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-				-- the name of the parser)
-				-- list of language that will be disabled
-				disable = {},
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-		}
+		indent = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
