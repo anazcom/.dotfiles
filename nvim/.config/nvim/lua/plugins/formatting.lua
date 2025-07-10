ff397fb18b21f7138016070ec7f013273ff61ee1
anazcom <tonito.nazco@gmail.com>
2025-07-10 19:43:32 -0400

    feat(formatting) updated conform configuration for python using ruff

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -14,9 +14,12 @@ return {
 	},
 	opts = {
 		formatters_by_ft = {
-
 			lua = { "stylua" },
-			python = { "ruff" },
+			python = {
+				"ruff_fix", -- To fix auto-fixable lint errors.
+				"ruff_format", -- To run the Ruff formatter.
+				"ruff_organize_imports", -- To organize the imports.
+			},
 			javascript = { "prettier", stop_after_first = true },
 			typescript = { "prettier", stop_after_first = true },
 			html = { "prettier", stop_after_first = true },
